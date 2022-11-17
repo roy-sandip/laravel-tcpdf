ebcd4a831b3a9e1576466e2fad555cf327106a3f
Sandip Roy <sandiproy876@gmail.com>
2022-11-17 18:20:43 +0600

    Updated

--- src/Facades/PDF.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace RoySandip\LaravelTcpdf\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-class PDF extends Facade
-{
-	protected static function getFacadeAccessor(){return 'pdf';}
-}
